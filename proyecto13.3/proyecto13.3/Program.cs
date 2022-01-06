33d01c63a5fca177f691142a151aa3d0a599f8fa
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2022-01-06 11:21:57 -0800

    version1.0

---
+++ proyecto13.3/proyecto13.3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto13._3
+{
+    class Program
+    {
+        public class Plano
+        {
+            private float x, y;
+
+            public void CargarPunto()
+            {
+                Console.Write("Dame el valor del punto x: ");
+                x = float.Parse(Console.ReadLine());
+
+                Console.Write("Dame el valor del punto x: ");
+                y = float.Parse(Console.ReadLine());
+            }
+
+            public void CuadrantePunto()
+            {
+                if (x < 0 && y > 0)
+                {
+                    Console.Write("Primer ");
+                }
+                else if(x > 0 && y > 0)
+                {
+                    Console.Write("Segundo ");
+                }
+                else if(x > 0 && y < 0)
+                {
+                    Console.Write("Tercer ");
+                }
+                else if(x < 0 && y < 0)
+                {
+                    Console.Write("Cuarto ");
+                }
+                else
+                {
+                    Console.Write("Ningun ");
+                }
+                Console.WriteLine("cuadrante.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Plano punto = new Plano();
+                punto.CargarPunto();
+                punto.CuadrantePunto();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un dato invalido.");                
+            }
+            Console.ReadKey();
+        }
+    }
+}
